ff2f4e014872133daf8a4091ac64ad6d718e936c
Dafe-akaka <dafe.akaka123@gmail.com>
2022-03-10 16:06:41 +0000

    projects gifs

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import profile from './Images/Profile.png'
 
 const About = () => {
   return (
@@ -6,33 +7,33 @@ const About = () => {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi, I'm Reed.
-            <br className="hidden lg:inline-block" />I love to build amazing
-            apps.
+            Hi, I'm Dafé.
+            <br className="hidden lg:inline-block" />
+            Full-Stack Software Engineer and Economics Graduate.
           </h1>
           <p className="mb-8 leading-relaxed">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-            laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-            Laborum, voluptas natus?
+                I enjoy building applications, and
+                relish the opprtunity to collaborate with others on projects.
           </p>
           <div className="flex justify-center">
             <a
-              href="#contact"
+              
+              href="#projects"
               className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
-              Work With Me
+              See My Past Work
             </a>
             <a
-              href="#projects"
+              href="#contact"
               className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
-              See My Past Work
+              Work With Me
             </a>
           </div>
         </div>
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
           <img
             className="object-cover object-center rounded"
-            alt="hero"
-            src="./coding.svg"
+            alt="profile"
+            src={profile}
           />
            </div>
       </div>
--- src/data.js
+++ src/data.js
@@ -1,34 +1,39 @@
+import hommer from "./Components/Images/hommer.gif"
+import reinhabit from "./Components/Images/reinhabit.gif"
+import tvShows from "./Components/Images/tv_shows.png"
+import subCity from "./Components/Images/sub-city.gif"
+
 export const projects = [
     {
-      title: "React Reserve",
-      subtitle: "MERN Stack",
+      title: "Sub-city Events",
+      subtitle: "PERN Stack with TypeScript",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: "./project-1.gif",
-      link: "https://reactbootcamp.com",
+        "Event planning app with stripe payment api allows the user to plan custom events and distribute details and invitations between third parties via a link",
+      image: subCity,
+      link: "https://sub-city.netlify.app/",
     },
     {
-      title: "React Tracks",
-      subtitle: "React and Python",
+      title: "Code Runner",
+      subtitle: "JavaScript & Node.js",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: "./project-2.gif",
-      link: "https://reedbarger.com",
+        "An app where users can anonymously post journal entries for other people to see, comment on and react to using emojis.",
+      image: hommer,
+      link: "https://coderunners.netlify.app/",
     },
     {
-      title: "DevChat",
-      subtitle: "React and Firebase",
+      title: "Reinhabit",
+      subtitle: "JavaScript and Postgress",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: "./project-3.gif",
-      link: "https://jsbootcamp.com",
+        "Habit tracking app where users can make an account and create habits for themselves to track and update with visual data.",
+      image: reinhabit,
+      link: "https://reinhabit.netlify.app",
     },
     {
         title: "Epic Todo App",
         subtitle: "React Hooks",
         description:
           "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-        image: "./project-4.gif",
-        link: "https://pythonbootcamp.com",
+        image: tvShows,
+        link: "https://compassionate-agnesi-6be84b.netlify.app/",
       },
     ];
