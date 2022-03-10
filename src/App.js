5856aa6a3c96908fbcbd3c54f14421d61ce00ca6
Dafe-akaka <dafe.akaka123@gmail.com>
2022-03-10 13:34:30 +0000

    nav bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './Components/Navbar';
+import About from './Components/About';
+// import Skills from './Components/Skills';
+import Projects from './Components/Projects';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar/>
+      <About/>
+      <Projects/>
+      {/* <Skills/> */}
+    </main>
   );
 }
 
